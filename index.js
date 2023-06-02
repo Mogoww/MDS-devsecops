ab6fd5c797f940b320058225837071a46203fcc2
Gauthier <gauthier.mauche@gmail.com>
2023-06-02 12:08:19 +0200

    up

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function afficherNom() {
-    var nom = document.getElementById("nom").value;
-    document.write("<p>Bonjour " + nom + "!</p>");
-  }
+// function afficherNom() {
+//     var nom = document.getElementById("nom").value;
+//     document.write("<p>Bonjour " + nom + "!</p>");
+//   }
 
-  document.getElementById("bouton").addEventListener("click", afficherNom);
+//   document.getElementById("bouton").addEventListener("click", afficherNom);
+
+let titleElement = document.getElementById("title");
+let name = new URLSearchParams(window.location.search).get("name");
+if (name !== null) {
+  titleElement.innerHTML = `Hello name`;
+}
